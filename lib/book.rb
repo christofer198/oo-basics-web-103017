78e4a095931fa3b0c53bc5c17f42a4d693e7f22a
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-01 18:13:15 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,4 @@
 class Book
-  def initialize(title, author_name, page_count, genre)
-    @title = title
-    @author_name = author_name
-    @page_count = page_count
-    @genre = genre
+  def initialize(title)
   end
 end
