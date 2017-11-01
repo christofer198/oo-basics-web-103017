1b0b4cc50cb2b68ffceb263a500e0b38613f5631
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-01 18:19:40 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,17 +1,13 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
   def initialize(title)
     @title = title
   end
 
-  def title
-    @title
-  end
-
-  attr_accessor :author
-  attr_accessor :page_count
-  attr_accessor :genre
-
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
 end
